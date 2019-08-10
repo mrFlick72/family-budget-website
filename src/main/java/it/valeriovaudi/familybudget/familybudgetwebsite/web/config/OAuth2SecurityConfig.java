d6beeaf7a92073f23c3e3c5474a3ab1419e338c1
mrFlick72 <valerio.vaudi@gmail.com>
2019-08-10 12:27:46 +0200

    VAuthenticator custom oauth2 user

--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/OAuth2SecurityConfig.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/OAuth2SecurityConfig.java
@@ -1,6 +1,7 @@
 package it.valeriovaudi.familybudget.familybudgetwebsite.web.config;
 
-import org.springframework.context.annotation.Configuration;
+import it.valeriovaudi.familybudget.familybudgetwebsite.web.security.VAuthenticatorOAuth2User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,11 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.oauth2.client.registration.client.client-id}")
+    private String familyBudgetClientRegistrationId;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().mvcMatchers("/actuator/**").permitAll().and()
                 .authorizeRequests().anyRequest().authenticated()
-                .and().oauth2Login();
+                .and().oauth2Login()
+                .userInfoEndpoint()
+                .customUserType(VAuthenticatorOAuth2User.class, familyBudgetClientRegistrationId);
     }
 }

a383d031bbd3a8f287f8f94a0d6bc7960730806b
mrFlick72 <valerio.vaudi@gmail.com>
2019-09-14 00:27:40 +0200

    openId Connect front end global logout

--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/OAuth2SecurityConfig.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/OAuth2SecurityConfig.java
@@ -15,7 +15,7 @@ public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().mvcMatchers("/actuator/**").permitAll().and()
+                .authorizeRequests().mvcMatchers("/actuator/**", "/oidc_logout.html").permitAll().and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and().oauth2Login().defaultSuccessUrl("/index")
                 .userInfoEndpoint()

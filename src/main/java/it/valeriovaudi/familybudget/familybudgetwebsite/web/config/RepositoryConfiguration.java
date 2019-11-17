5e81c7ad067dde0740f3611390210a5f386a4722
mrFlick72 <valerio.vaudi@gmail.com>
2019-11-17 13:41:52 +0100

    message repository on i18n service

--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/RepositoryConfiguration.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/RepositoryConfiguration.java
@@ -1,24 +1,15 @@
 package it.valeriovaudi.familybudget.familybudgetwebsite.web.config;
 
 
-import it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository.JpaDictionaryMessageRepository;
 import it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository.RestMessageRepository;
-import it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository.delegate.SpringDataJpaDictionaryMessageRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Locale;
-
 @Configuration
 public class RepositoryConfiguration {
-
-    @Bean
-    public JpaDictionaryMessageRepository messagesRepository(SpringDataJpaDictionaryMessageRepository springDataJpaDictionaryMessageRepository) {
-        return new JpaDictionaryMessageRepository(Locale.ENGLISH, springDataJpaDictionaryMessageRepository);
-    }
 
     @Bean
     @LoadBalanced
--- src/main/resources/postgres_schema.sql
+++
@@ -1,9 +0,0 @@
-create table dictionary_message
-(
-	locale varchar(255) not null,
-	message_key varchar(255) not null,
-	page varchar(255) not null,
-	value varchar(255),
-	constraint dictionary_message_pkey
-		primary key (locale, message_key, page)
-);

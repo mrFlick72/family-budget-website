00368983fdadb8dc017278a714e56a18f0a13138
mrflick72 <valerio.vaudi@gmail.com>
2020-04-16 00:22:00 +0200

    remove import/export from file csv useless feature

--- src/main/frontend/app/component/menu/ExportImportMenuItem.js
+++ src/main/frontend/app/component/menu/ExportImportMenuItem.js
@@ -3,22 +3,16 @@ import SingleMenuItem from "./SingleMenuItem";
 
 export default ({
                     downloadLink,
-                    downloadStentBudgetAsFileUseCaseCsvHandler,
                     downloadStentBudgetAsFileUseCasePdfHandler,
                     downloadStentBudgetAsFileUseCaseXlsxHandler
                 }) => {
     return <li className="nav-item dropdown active">
         <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#"
            role="button" aria-haspopup="true" aria-expanded="false">
-            <i className="fas fa-exchange-alt fa-lg"></i>Import/Export
+            <i className="fas fa-exchange-alt fa-lg"></i>Export
         </a>
         <div className="dropdown-menu">
             <a className="hide" ref={downloadLink} href="#"/>
-            <SingleMenuItem key='csv'
-                            menuItemLabel='Export To CSV'
-                            menuItemPrefixIcon={<i className="fas fa-download fa-lg"></i>}
-                            onClickHandler={downloadStentBudgetAsFileUseCaseCsvHandler}/>
-
             <SingleMenuItem key='pdf'
                             menuItemLabel='Export To Pdf'
                             menuItemPrefixIcon={<i className="fas fa-download fa-lg"></i>}
@@ -28,10 +22,6 @@ export default ({
                             menuItemLabel='Export To Excel'
                             menuItemPrefixIcon={<i className="fas fa-download fa-lg"></i>}
                             onClickHandler={downloadStentBudgetAsFileUseCaseXlsxHandler}/>
-
-            <a href="#" className="dropdown-item" data-toggle="modal" data-target=".loadCsvFile">
-                <i className="fas fa-upload fa-lg"></i> Load By CSV
-            </a>
         </div>
     </li>
 }

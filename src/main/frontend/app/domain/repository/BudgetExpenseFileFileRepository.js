ddab240f30e4aa36dcc4a8d94659652f94c1013c
mrFlick72 <valerio.vaudi@gmail.com>
2020-11-20 16:21:56 +0100

    explicit multipart/form-data on load attachment ui repository

--- src/main/frontend/app/domain/repository/BudgetExpenseFileFileRepository.js
+++ src/main/frontend/app/domain/repository/BudgetExpenseFileFileRepository.js
@@ -9,6 +9,9 @@ export class BudgetExpenseFileFileRepository {
         return fetch(BUDGET_EXPENSE_ATTACHMENT_URI(budgetExpenseId), {
             method: "POST",
             body: data,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
             credentials: 'same-origin'
         })
     }

472e34aa02af213e350a4a247c9d95f8d6ce5304
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-02 14:53:45 +0100

    cache invalidator listener

--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/adapters/repository/I18nMessagesCacheRefresher.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/adapters/repository/I18nMessagesCacheRefresher.java
@@ -1,23 +1,66 @@
 package it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.cache.CacheManager;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageResponse;
 
-import java.util.HashMap;
+import java.time.Duration;
+import java.util.List;
 import java.util.Objects;
 
-@Deprecated
-public class I18nMessagesCacheRefresher {
+import static reactor.core.publisher.Mono.fromCompletionStage;
+
+public class I18nMessagesCacheRefresher implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(I18nMessagesCacheRefresher.class);
 
     private final CacheManager cacheManager;
+    private final ReceiveMessageRequestFactory factory;
+    private final Duration sleepingTime;
+    private final Flux whileLoopFluxProvider;
+    private final SqsAsyncClient sqsAsyncClient;
 
-    public I18nMessagesCacheRefresher(CacheManager cacheManager) {
+    public I18nMessagesCacheRefresher(
+            CacheManager cacheManager,
+            Duration sleepingTime,
+            Flux whileLoopFluxProvider,
+            ReceiveMessageRequestFactory factory,
+            SqsAsyncClient sqsAsyncClient) {
         this.cacheManager = cacheManager;
+        this.sleepingTime = sleepingTime;
+        this.whileLoopFluxProvider = whileLoopFluxProvider;
+        this.factory = factory;
+        this.sqsAsyncClient = sqsAsyncClient;
     }
 
-    public Mono<Void> refresh(HashMap<String, String> bundle) {
-        return Mono.fromCallable(() -> Objects.requireNonNull(cacheManager.getCache("family-budget-website.i18n.messages")).invalidate())
-                .map(tick -> Mono.fromCallable(() -> cacheManager.getCache("family-budget-website.i18n.messages").putIfAbsent("i18n.messages", bundle)))
-                .then();
+    public Flux listen() {
+        return whileLoopFluxProvider
+                .delayElements(sleepingTime)
+                .log()
+                .flatMap(req -> handleMessage())
+                .flatMap(signal -> Mono.fromCallable(() -> Objects.requireNonNull(cacheManager.getCache("family-budget-website.i18n.messages")).invalidate()))
+                .doOnComplete(() -> logger.info("subscription completed"))
+                .doOnCancel(() -> logger.info("subscription cancelled"))
+                .doOnSubscribe((s) -> logger.info("subscription started"))
+                .doOnError(Exception.class, (e) -> logger.error("subscription error: ", e));
     }
+
+    private Mono<List<DeleteMessageResponse>> handleMessage() {
+        return Flux.from(fromCompletionStage(sqsAsyncClient.receiveMessage(factory.makeAReceiveMessageRequest())))
+                .flatMap(response -> Flux.fromIterable(response.messages()))
+                .flatMap(message -> fromCompletionStage(sqsAsyncClient.deleteMessage(factory.makeADeleteMessageRequest(message.receiptHandle()))))
+                .collectList();
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        listen().subscribe();
+    }
+
 }
--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/RepositoryConfiguration.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/RepositoryConfiguration.java
@@ -1,11 +1,22 @@
 package it.valeriovaudi.familybudget.familybudgetwebsite.web.config;
 
 
+import it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository.I18nMessagesCacheRefresher;
+import it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository.ReceiveMessageRequestFactory;
 import it.valeriovaudi.familybudget.familybudgetwebsite.adapters.repository.RestMessageRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Flux;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RepositoryConfiguration {
@@ -23,4 +34,37 @@ public class RepositoryConfiguration {
         return new RestMessageRepository(i18nBaseUrl, applicationId, restTemplate);
     }
 
+    @Bean
+    public ReceiveMessageRequestFactory receiveMessageRequestFactory(@Value("${i18n-messages.cache.updater.listener.queueUrl}") String queueUrl,
+                                                                     @Value("${i18n-messages.cache.updater.listener.maxNumberOfMessages}") Integer maxNumberOfMessages,
+                                                                     @Value("${i18n-messages.cache.updater.listener.visibilityTimeout}") Integer visibilityTimeout,
+                                                                     @Value("${i18n-messages.cache.updater.listener.waitTimeSeconds}") Integer waitTimeSeconds
+
+    ) {
+        return new ReceiveMessageRequestFactory(queueUrl, maxNumberOfMessages, visibilityTimeout, waitTimeSeconds);
+    }
+
+    @Bean
+    public I18nMessagesCacheRefresher reactiveCacheUpdaterListener(@Value("${i18n-messages.cache.updater.listener.sleeping:10m}") Duration sleeping,
+                                                                   CacheManager cacheManager,
+                                                                   ReceiveMessageRequestFactory receiveMessageRequestFactory,
+                                                                   SqsAsyncClient sqsAsyncClient) {
+        return new I18nMessagesCacheRefresher(cacheManager, sleeping, Flux.just(1).repeat(), receiveMessageRequestFactory, sqsAsyncClient);
+    }
+
+    @Bean
+    public AwsCredentialsProvider awsCredentialsProvider(@Value("${aws.access-key}") String accessKey,
+                                                         @Value("${aws.secret-key}") String awsSecretKey) {
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, awsSecretKey));
+    }
+
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient(@Value("${aws.region}") String awsRegion,
+                                         AwsCredentialsProvider awsCredentialsProvider) {
+        return SqsAsyncClient.builder()
+                .credentialsProvider(awsCredentialsProvider)
+                .region(Region.of(awsRegion))
+                .build();
+    }
 }

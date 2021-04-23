0f89e47396674a17de078b2e88892c1a6472b223
mrFlick72 <valerio.vaudi@gmail.com>
2021-04-23 16:15:31 +0200

    log when cache is refreshed

--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/adapters/repository/I18nMessagesCacheRefresher.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/adapters/repository/I18nMessagesCacheRefresher.java
@@ -44,7 +44,11 @@ public class I18nMessagesCacheRefresher implements ApplicationRunner {
                 .delayElements(sleepingTime)
                 .log()
                 .flatMap(req -> handleMessage())
-                .flatMap(signal -> Mono.fromCallable(() -> Objects.requireNonNull(cacheManager.getCache("family-budget-website.i18n.messages")).invalidate()))
+                .flatMap(signal -> Mono.fromCallable(() -> {
+                            logger.info("cache refresh event received ");
+                            return Objects.requireNonNull(cacheManager.getCache("family-budget-website.i18n.messages")).invalidate();
+                        }
+                ))
                 .doOnComplete(() -> logger.info("subscription completed"))
                 .doOnCancel(() -> logger.info("subscription cancelled"))
                 .doOnSubscribe((s) -> logger.info("subscription started"))

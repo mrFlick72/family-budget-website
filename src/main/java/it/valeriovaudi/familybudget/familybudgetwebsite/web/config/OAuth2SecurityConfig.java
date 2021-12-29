f9f299a330360ba9ebb250b510528d5f0120a6fb
mrflick72 <valerio.vaudi@gmail.com>
2021-12-29 19:20:38 +0100

    jdk 17 build upgrade

--- src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/OAuth2SecurityConfig.java
+++ src/main/java/it/valeriovaudi/familybudget/familybudgetwebsite/web/config/OAuth2SecurityConfig.java
@@ -31,7 +31,7 @@ public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().disable().headers().frameOptions().sameOrigin().and()
                 .authorizeRequests().mvcMatchers("/actuator/**", "/oidc_logout.html").permitAll()
                 .and()
                 .authorizeRequests().anyRequest().hasAnyRole(grantedRole)

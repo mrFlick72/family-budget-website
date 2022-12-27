5003b301fc8a34f3aa41464593d6a3894abf5e15
mrflick72 <valerio.vaudi@gmail.com>
2022-12-28 00:32:48 +0100

    create new budget popup v2 first commit

---
+++ src/main/frontend/app/family-budget-site/spent-budget/popup/CreateNewBudgetExpensePopupv2.js
@@ -0,0 +1,47 @@
+import React from "react"
+import SpentBudgetForm from "../budget/SpentBudgetForm";
+import YesAndNoButtonGroup from "../../../component/layout/YesAndNoButtonGroup";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
+
+const CreateNewBudgetExpensePopupV2 = ({
+                                           open,
+                                           modal,
+                                           searchTagRegistry,
+                                           spentBudgetHandlers,
+                                           budgetExpense,
+                                           saveCallback
+                                       }) => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>{modal.title}</DialogTitle>
+
+        <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                <SpentBudgetForm spentBudgetData={budgetExpense}
+                                 spentBudgetHandlers={spentBudgetHandlers}
+                                 searchTagRegistry={searchTagRegistry.map(searchTag => {
+                                     return {value: searchTag.key, label: searchTag.value}
+                                 })}/>
+            </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+            <YesAndNoButtonGroup yesIcon="fas fa-cart-plus fa-lg"
+                                 yesFun={saveCallback}
+                                 buttonMessages={{
+                                     "noLabel": modal.closeButtonLable,
+                                     "yesLabel": modal.saveButtonLable
+                                 }}/>
+        </DialogActions>
+    </Dialog>
+}
+
+export default CreateNewBudgetExpensePopupV2
